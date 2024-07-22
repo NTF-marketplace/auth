2df6731294d0c02bfaf7132640fa538549a4434a
min96 <ajdcnd131@naver.com>
2024-07-22 18:10:54 +0900

    change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ group = "com.api"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -56,7 +56,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
