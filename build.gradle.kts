082446f5751412d48813248e0e19fe7802d2b2db
min96 <ajdcnd131@naver.com>
2024-07-23 01:37:56 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,7 @@ group = "com.api"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
